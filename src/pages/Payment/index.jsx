915a95f763f9f12aee5657fe35a68ea9ddb87360
Thariq Farsha <mthariqfarsha@gmail.com>
2022-04-27 16:09:41 +0700

    Create payment page

---
+++ src/pages/Payment/index.jsx
@@ -0,0 +1,135 @@
+import React from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import Footer from "../../components/Footer";
+import Navbar from "../../components/Navbar";
+
+function Payment() {
+  const navigate = useNavigate();
+  const { state } = useLocation();
+  console.log(state);
+
+  const handlePayment = (dataPayment) => {
+    console.log(dataPayment);
+  };
+
+  return (
+    <>
+      <Navbar />
+
+      <main className="bg-primary bg-opacity-10 pb-4">
+        <div className="container-lg">
+          <div className="row">
+            <div className="col-md-7 p-3">
+              <div className="payment-info-group mb-0 mb-md-4">
+                <h2 className="h4 fw-bold mb-3">Payment Info</h2>
+                <div className="card p-2 p-md-3">
+                  <ul className="list-group list-group-flush">
+                    <li className="list-group-item py-3 d-block d-md-flex justify-content-between align-items-center">
+                      <span className="d-block text-secondary">Date & time</span>
+                      <span className="d-block text-md-end fw-semibold">
+                        {state.dateBooking} at {state.timeBooking}
+                      </span>
+                    </li>
+                    <li className="list-group-item py-3 d-block d-md-flex justify-content-between align-items-center">
+                      <span className="d-block text-secondary">Movie title</span>
+                      <span className="d-block text-md-end fw-semibold">{state.movieName}</span>
+                    </li>
+                    <li className="list-group-item py-3 d-block d-md-flex justify-content-between align-items-center">
+                      <span className="d-block text-secondary">Cinema</span>
+                      <span className="d-block text-md-end fw-semibold">{state.premiere}</span>
+                    </li>
+                    <li className="list-group-item py-3 d-block d-md-flex justify-content-between align-items-center">
+                      <span className="d-block text-secondary">Number of tickets</span>
+                      <span className="d-block text-md-end fw-semibold">
+                        {state.seatBooking.length} pieces
+                      </span>
+                    </li>
+                    <li className="list-group-item py-3 d-block d-md-flex justify-content-between align-items-center">
+                      <span className="d-block text-secondary">Total Payment</span>
+                      <span className="d-block text-md-end fw-semibold">
+                        Rp {state.totalPayment}
+                      </span>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="d-none d-md-flex justify-content-between align-items-center">
+                <button
+                  className="btn btn-outline-primary py-2 shadow-sm d-none d-md-block"
+                  onClick={() => navigate(-1)}
+                >
+                  Change Seats
+                </button>
+                <button
+                  className="checkout-btn btn btn-primary py-2 fw-bold shadow"
+                  onClick={() => handlePayment({ ...state })}
+                >
+                  Proceed to Pay
+                </button>
+              </div>
+            </div>
+            <div className="col-md-5 p-3">
+              <div className="personal-info-group mb-3">
+                <h2 className="h4 fw-bold mb-3">Personal info</h2>
+                <div className="card p-4">
+                  <div className="mb-3">
+                    <label htmlFor="full-name" className="form-label">
+                      Full Name
+                    </label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="full-name"
+                      placeholder="Full name"
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <label htmlFor="email" className="form-label">
+                      Email
+                    </label>
+                    <input
+                      type="email"
+                      className="form-control"
+                      id="email"
+                      placeholder="email@example.com"
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="phone-number" className="form-label">
+                      Phone Number
+                    </label>
+                    <input
+                      type="tel"
+                      minLength="11"
+                      maxLength="13"
+                      className="form-control"
+                      id="phone-number"
+                      placeholder="08XXXXXXXXXX"
+                    />
+                  </div>
+                  {/* <div className="alert alert-warning mt-4 mb-0" role="alert">
+                    <i className="bi bi-exclamation-triangle-fill me-2"></i>Fill your data
+                    correctly!
+                  </div> */}
+                </div>
+              </div>
+            </div>
+            <div className="col-md-7 px-3 pb-3 d-md-none d-grid">
+              <button
+                className="checkout-btn btn btn-primary py-2 fw-bold shadow"
+                onClick={() => handlePayment({ ...state })}
+              >
+                Proceed to Pay
+              </button>
+            </div>
+          </div>
+          <div className="row"></div>
+        </div>
+      </main>
+
+      <Footer />
+    </>
+  );
+}
+
+export default Payment;

3101a7ec09da5c8fdba191100efe83d301cf36d7
Thariq Farsha <mthariqfarsha@gmail.com>
2022-04-28 09:15:02 +0700

    Set dynamic card content using props

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,18 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./index.css";
 
-function Card() {
+function Card(props) {
+  console.log("props", props.data);
   return (
-    <div className="card d-inline-block text-center p-3 p-md-4">
+    <div className="movie-card card d-inline-block text-center p-3 p-md-4">
       <img
-        src={require("../../assets/img/movie/thumb/spiderman-thumb.png")}
-        className="card-img-top"
+        src={props.data ? props.data.imagePath : ""}
+        className="card-img-top rounded"
         alt="spiderman"
       />
       <div className="card-body p-0 pt-3">
-        <h5 className="card-title">Movie Title</h5>
-        <p className="card-text text-dark text-opacity-50 pb-3">Category</p>
-        <Link to="#" className="btn btn-outline-primary py-1 w-100">
+        <h5 className="h6 fw-semibold card-title text-wrap">{props.data ? props.data.name : ""}</h5>
+        <p className="card-text text-dark text-opacity-50 pb-3">
+          {props.data ? props.data.category : ""}
+        </p>
+        <Link
+          to={`detail/${props.data ? props.data.id : ""}`}
+          className="btn btn-outline-primary py-1 w-100"
+        >
           Details
         </Link>
       </div>

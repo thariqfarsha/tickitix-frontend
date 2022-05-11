119298261af9da6b66df2bb0e0df2d2427422eed
Thariq Farsha <mthariqfarsha@gmail.com>
2022-05-12 01:24:41 +0700

    Add image placeholder and default title & category

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./index.css";
 
 function Card(props) {
   console.log("props", props.data);
   return (
     <div className="movie-card card d-inline-block text-center p-3 p-md-4">
       <img
-        src={props.data ? props.data.imagePath : ""}
+        src={props.data ? props.data.imagePath : "https://via.placeholder.com/300x400.png?text=+"}
         className="card-img-top rounded"
-        alt="spiderman"
+        alt={props.data ? props.data.name : ""}
       />
       <div className="card-body p-0 pt-3">
-        <h5 className="h6 fw-semibold card-title text-wrap">{props.data ? props.data.name : ""}</h5>
+        <h5 className="h6 fw-semibold card-title text-wrap">
+          {props.data ? props.data.name : "Movie Title"}
+        </h5>
         <p className="card-text text-dark text-opacity-50 pb-3">
-          {props.data ? props.data.category : ""}
+          {props.data ? props.data.category : "Movie Category"}
         </p>
         <Link
           to={`detail/${props.data ? props.data.id : ""}`}

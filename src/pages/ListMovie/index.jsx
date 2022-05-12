79f98e9b91794eb1b41a3b1d34f7d8a51783996a
Thariq Farsha <mthariqfarsha@gmail.com>
2022-05-13 00:44:45 +0700

    Integrate list movie page to backend

--- src/pages/ListMovie/index.jsx
+++ src/pages/ListMovie/index.jsx
@@ -1,17 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactPaginate from "react-paginate";
 import Card from "../../components/Card";
 import Footer from "../../components/Footer";
 import MonthFilter from "../../components/MonthFilter";
 import Navbar from "../../components/Navbar";
+import { useSelector, useDispatch } from "react-redux";
+import { getDataMovieRedux } from "../../stores/action/movie";
 
 function ListMovie() {
-  const pageInfo = {
-    totalPage: 4
+  const dispatch = useDispatch();
+  const movies = useSelector((state) => state.movie);
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    getDataMovie();
+  }, []);
+
+  useEffect(() => {
+    getDataMovie();
+  }, [page]);
+
+  const getDataMovie = async () => {
+    try {
+      dispatch(getDataMovieRedux(page));
+    } catch (error) {
+      console.log(error.response);
+    }
   };
 
-  const handlePagination = () => {
-    return;
+  const handlePagination = (e) => {
+    setPage(e.selected + 1);
   };
 
   return (
@@ -45,39 +63,28 @@ function ListMovie() {
         <MonthFilter />
         <div className="container-lg">
           <div className="card border-0 p-5">
-            <div className="row g-5 px-3 py-2">
-              <div className="col-3">
-                <Card />
+            {movies.isLoading ? (
+              <div className="d-flex justify-content-center">
+                <div className="spinner-grow text-primary text-center" role="status">
+                  <span className="visually-hidden">Loading...</span>
+                </div>
               </div>
-              <div className="col-3">
-                <Card />
+            ) : (
+              <div className="row row-cols-4 g-5 px-3 py-2">
+                {movies.data.map((movie) => (
+                  <div className="col" key={movie.id}>
+                    <Card data={movie} />
+                  </div>
+                ))}
               </div>
-              <div className="col-3">
-                <Card />
-              </div>
-              <div className="col-3">
-                <Card />
-              </div>
-              <div className="col-3">
-                <Card />
-              </div>
-              <div className="col-3">
-                <Card />
-              </div>
-              <div className="col-3">
-                <Card />
-              </div>
-              <div className="col-3">
-                <Card />
-              </div>
-            </div>
+            )}
           </div>
           <div className="d-flex justify-content-center mt-4 mb-2">
             <ReactPaginate
               previousLabel={"Previous"}
               nextLabel={"Next"}
               breakLabel={"..."}
-              pageCount={pageInfo.totalPage}
+              pageCount={movies.pageInfo.totalPage}
               onPageChange={handlePagination}
               containerClassName={"pagination"}
               pageClassName={"page-item px-1"}

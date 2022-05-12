f7e884adca0ecdcfd3200f4bfc5eef0adb446062
Thariq Farsha <mthariqfarsha@gmail.com>
2022-05-12 20:54:56 +0700

    Add logout func, make navbar dynamic & interactive

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,8 +1,14 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 
 function Navbar() {
+  const location = useLocation();
+  const navigate = useNavigate();
   const dataUser = JSON.parse(localStorage.getItem("dataUser"));
+
+  const handleLogout = () => {
+    localStorage.clear();
+  };
 
   return (
     <nav className="navbar navbar-expand-md navbar-light bg-white fixed-top text-center shadow-sm px-3 px-lg-0">
@@ -27,16 +33,64 @@ function Navbar() {
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav me-auto my-2">
+          <ul
+            className={`navbar-nav me-auto my-2 ${
+              dataUser?.role === "admin" ? "d-none" : "d-flex"
+            }`}
+          >
             <li className="nav-item">
-              <Link to="/" className="nav-link active" aria-current="page">
+              <Link
+                to="/"
+                className={`nav-link ${location.pathname === "/" ? "text-primary" : ""}`}
+                aria-current="page"
+              >
                 Home
               </Link>
             </li>
             <hr className="my-1" />
             <li className="nav-item">
-              <Link to="#" className="nav-link">
+              <Link
+                to="/list-movie"
+                className={`nav-link ${location.pathname === "/list-movie" ? "text-primary" : ""}`}
+              >
                 List Movie
+              </Link>
+            </li>
+            <hr className="my-1" />
+          </ul>
+          <ul
+            className={`navbar-nav me-auto my-2 ${
+              dataUser?.role === "admin" ? "d-flex" : "d-none"
+            }`}
+          >
+            <li className="nav-item">
+              <Link
+                to="/dashboard"
+                className={`nav-link ${location.pathname === "/dashboard" ? "text-primary" : ""}`}
+              >
+                Dashboard
+              </Link>
+            </li>
+            <hr className="my-1" />
+            <li className="nav-item">
+              <Link
+                to="/manage-movie"
+                className={`nav-link ${
+                  location.pathname === "/manage-movie" ? "text-primary" : ""
+                }`}
+              >
+                Manage Movie
+              </Link>
+            </li>
+            <hr className="my-1" />
+            <li className="nav-item">
+              <Link
+                to="/manage-schedule"
+                className={`nav-link ${
+                  location.pathname === "/manage-schedule" ? "text-primary" : ""
+                }`}
+              >
+                Manage Schedule
               </Link>
             </li>
             <hr className="my-1" />
@@ -52,13 +106,38 @@ function Navbar() {
             <div>
               <i className="bi bi-search"></i>
             </div>
-            <div className="profilePicture">
-              <img
-                src={dataUser ? dataUser.imagePath : ""}
-                alt="profile"
-                className="rounded-circle ms-5"
-                style={{ width: "44px" }}
-              />
+
+            <div className="dropdown ms-5">
+              <button
+                className="bg-transparent border-0 dropdown-toggle"
+                type="button"
+                id="dropdownMenuButton1"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+                data-bs-offset="  0,20"
+              >
+                <img
+                  src={dataUser ? dataUser.imagePath : ""}
+                  alt="profile"
+                  className="rounded-circle"
+                  style={{ width: "44px" }}
+                />
+              </button>
+              <ul
+                className="dropdown-menu dropdown-menu-end rounded-3"
+                aria-labelledby="dropdownMenuButton1"
+              >
+                <li>
+                  <Link className="dropdown-item" to="#">
+                    Profile
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/" className="dropdown-item" onClick={handleLogout}>
+                    Logout
+                  </Link>
+                </li>
+              </ul>
             </div>
           </div>
         </div>

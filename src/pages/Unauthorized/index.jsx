356dd647fe749e3ea1d457e4c2236588bab483df
Thariq Farsha <mthariqfarsha@gmail.com>
2022-05-16 20:02:47 +0700

    Add style for unauthorized page

--- src/pages/Unauthorized/index.jsx
+++ src/pages/Unauthorized/index.jsx
@@ -1,18 +1,27 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import Navbar from "../../components/Navbar";
 
 export default function Unauthorized() {
   const navigate = useNavigate();
   return (
     <div>
-      Unauthorized
-      <button
-        onClick={() => {
-          navigate(-1);
-        }}
-      >
-        Back
-      </button>
+      <Navbar />
+
+      <main className="bg-primary-light pb-4 vh-100 d-flex align-items-center">
+        <div className="container-lg text-center mb-5">
+          <h1 className="display-1 fw-bold text-center">401</h1>
+          <p className="h2 fw-semibold mb-5 text-center">Your request is unauthorized</p>
+          <button
+            className="btn btn-primary fw-semibold"
+            onClick={() => {
+              navigate("/");
+            }}
+          >
+            Back to Home
+          </button>
+        </div>
+      </main>
     </div>
   );
 }

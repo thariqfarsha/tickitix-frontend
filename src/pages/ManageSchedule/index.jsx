c2a7c57e66841ac78253d40e51e5f4a45a80af43
Thariq Farsha <mthariqfarsha@gmail.com>
2022-05-17 23:31:58 +0700

    Add get schedule redux, make form functional

--- src/pages/ManageSchedule/index.jsx
+++ src/pages/ManageSchedule/index.jsx
@@ -1,33 +1,106 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Footer from "../../components/Footer";
 import Navbar from "../../components/Navbar";
 import Card from "../../components/Card";
 import ReactPaginate from "react-paginate";
 import { useSelector, useDispatch } from "react-redux";
 import { getDataMovieRedux } from "../../stores/action/movie";
+import {
+  getDataScheduleRedux,
+  postScheduleRedux,
+  updateScheduleRedux,
+  deleteScheduleRedux
+} from "../../stores/action/schedule";
+import ScheduleCard from "../../components/ScheduleCard";
+import { useLocation } from "react-router-dom";
 
 function ManageSchedule() {
+  const location = useLocation();
   const dispatch = useDispatch();
-  const movie = useSelector((state) => state.movie);
+  const movies = useSelector((state) => state.movie);
+  const schedules = useSelector((state) => state.schedule);
+
+  const [page, setPage] = useState(1);
+  const limit = 6;
+  const [preview, setPreview] = useState(null);
+  const [premiere, setPremiere] = useState("");
+  const [showInputTime, setShowInputTime] = useState(false);
+  const [form, setForm] = useState({
+    movieId: null,
+    premiere: "",
+    price: null,
+    location: "",
+    dateStart: "",
+    dateEnd: "",
+    time: []
+  });
+  const premiereList = ["ebv.id", "hiflix", "cineOne21"];
 
   useEffect(() => {
     getAllMovie();
   }, []);
 
+  useEffect(() => {
+    getScheduleByMovieId();
+  }, [form.movieId]);
+
   const getAllMovie = async () => {
     try {
-      dispatch(getDataMovieRedux());
+      await dispatch(getDataMovieRedux(1, 100));
     } catch (error) {
       console.log(error.response);
     }
   };
 
-  const pageInfo = {
-    totalPage: 4
+  const getScheduleByMovieId = async () => {
+    try {
+      await dispatch(getDataScheduleRedux(form.movieId, page, limit));
+    } catch (error) {
+      console.log(error.response);
+    }
   };
 
-  const handlePagination = () => {
+  const handlePagination = (e) => {
+    setPage(e.selected + 1);
+  };
+
+  const handleChangeForm = (e) => {
+    if (e.target.name === "movie-list") {
+      const dataMovie = movies.data.find((item) => {
+        return +item.id === +e.target.value;
+      });
+      setPreview(dataMovie ? dataMovie.imagePath : null);
+      setForm({ ...form, movieId: e.target.value });
+    }
+  };
+
+  const addTime = (e) => {
+    if (e.key === "Enter") {
+      setShowInputTime(false);
+      if (e.target.value === "") {
+        return;
+      }
+      setForm({
+        ...form,
+        time: [...form.time, e.target.value]
+      });
+    }
+  };
+
+  const handleSubmit = () => {
     return;
+  };
+
+  const resetForm = () => {
+    setForm({
+      movieId: null,
+      premiere: "",
+      price: null,
+      location: "",
+      dateStart: "",
+      dateEnd: "",
+      time: []
+    });
   };
 
   return (
@@ -38,134 +111,138 @@ function ManageSchedule() {
         <section className="form-movie container-lg mt-4 mb-5">
           <h2 className="h4 fw-bold mb-3">Form Schedule</h2>
           <div className="card border-0 p-5">
-            <div className="row gx-5 mb-4">
-              <div className="col-3">
-                <div className="card p-4">
-                  <img
-                    src={"https://via.placeholder.com/200x300.png?text=+"}
-                    alt="spiderman"
-                    className="img-fluid"
-                  />
+            <form onSubmit={handleSubmit} onReset={resetForm}>
+              <div className="row gx-5 mb-4">
+                <div className="col-3">
+                  <div className="card p-4">
+                    <img
+                      src={!preview ? "https://via.placeholder.com/200x300.png?text=+" : preview}
+                      alt="movie image preview"
+                    />
+                  </div>
                 </div>
-              </div>
-              <div className="col-9">
-                <div className="row">
-                  <div className="col-6">
-                    <div className="mb-4">
-                      <label htmlFor="movie-name" className="form-label">
-                        Movie Name
-                      </label>
-                      <select className="form-select" id="movie-name">
-                        <option defaultValue={""}>Select movie</option>
-                        <option value="Fantastic Beast">Fantastic Beast</option>
-                        <option value="Spiderman">Spiderman</option>
-                      </select>
-                    </div>
-                    <div className="mb-4">
-                      <label htmlFor="price" className="form-label">
-                        Price
-                      </label>
-                      <input type="text" className="form-control" id="price" />
-                    </div>
-                    <div className="mb-4">
-                      <label htmlFor="premiere" className="form-label">
-                        Premiere
-                      </label>
-                      <div className="row" id="premiere">
-                        <div className="col-4 d-flex align-items-center">
-                          <button className="btn" value={"ebv.id"}>
-                            <img
-                              src={require("../../assets/img/logo/cinema/ebv.id.png")}
-                              className="img-fluid"
-                              alt="ebv.id"
-                            />
-                          </button>
+                <div className="col-9">
+                  <div className="row">
+                    <div className="col-6">
+                      <div className="mb-4">
+                        <label htmlFor="movie-list" className="form-label">
+                          Movie Name
+                        </label>
+                        <select
+                          className="form-select"
+                          id="movie-list"
+                          name="movie-list"
+                          onChange={handleChangeForm}
+                        >
+                          <option defaultValue={""}>Select movie</option>
+                          {movies.data.map((movie) => (
+                            <option value={movie.id} key={movie.id}>
+                              {movie.name}
+                            </option>
+                          ))}
+                        </select>
+                      </div>
+                      <div className="mb-4">
+                        <label htmlFor="price" className="form-label">
+                          Price
+                        </label>
+                        <input type="text" className="form-control" id="price" />
+                      </div>
+                      <div className="mb-4">
+                        <label htmlFor="premiere" className="form-label">
+                          Premiere
+                        </label>
+                        <div className="row" id="premiere">
+                          {premiereList.map((el) => (
+                            <div className="col-4 d-flex align-items-center" key={el}>
+                              <button
+                                className={`btn ${el === premiere ? "shadow" : ""}`}
+                                value={el}
+                                onClick={() => setPremiere(el)}
+                                type="button"
+                              >
+                                <img
+                                  src={require(`../../assets/img/logo/cinema/${el}.png`)}
+                                  className="img-fluid"
+                                  alt={el}
+                                />
+                              </button>
+                            </div>
+                          ))}
                         </div>
-                        <div className="col-4 d-flex align-items-center">
-                          <button className="btn shadow" value={"hiflix"}>
-                            <img
-                              src={require("../../assets/img/logo/cinema/hiflix.png")}
-                              className="img-fluid"
-                              alt="hiflix"
-                            />
-                          </button>
+                      </div>
+                    </div>
+                    <div className="col-6">
+                      <div className="mb-4">
+                        <label htmlFor="location" className="form-label">
+                          Location
+                        </label>
+                        <select className="form-select" id="location">
+                          <option defaultValue={""}>Select location</option>
+                          <option value="East Purwokerto">East Purwokerto</option>
+                          <option value="West Purwokerto">West Purwokerto</option>
+                        </select>
+                      </div>
+                      <div className="row mb-4">
+                        <div className="col-6">
+                          <label htmlFor="date-start" className="form-label">
+                            Date Start
+                          </label>
+                          <input type="date" className="form-control" id="date-start" />
                         </div>
-                        <div className="col-4 d-flex align-items-center">
-                          <button className="btn" value={"cineOne21"}>
-                            <img
-                              src={require("../../assets/img/logo/cinema/cineOne21.png")}
-                              className="img-fluid"
-                              alt="cineOne21"
-                            />
-                          </button>
+                        <div className="col-6">
+                          <label htmlFor="date-end" className="form-label">
+                            Date End
+                          </label>
+                          <input type="date" className="form-control" id="date-end" />
+                        </div>
+                      </div>
+                      <div className="mb-4">
+                        <label htmlFor="location" className="form-label">
+                          Time
+                        </label>
+                        <div className="row row-cols-md-3 row-cols-lg-4 gy-3">
+                          <div className="col">
+                            {showInputTime ? (
+                              <input
+                                type="text"
+                                className="form-control py-1 w-100"
+                                onKeyPress={addTime}
+                                autoFocus={true}
+                              />
+                            ) : (
+                              <button
+                                className="btn btn-outline-primary w-100 py-1"
+                                onClick={() => setShowInputTime(true)}
+                              >
+                                <i className="bi bi-plus-lg"></i>
+                              </button>
+                            )}
+                          </div>
+                          {form.time.map((el) => (
+                            <div className="col">
+                              <button className="btn text-nowrap w-100 py-1">{el}</button>
+                            </div>
+                          ))}
                         </div>
                       </div>
                     </div>
                   </div>
-                  <div className="col-6">
-                    <div className="mb-4">
-                      <label htmlFor="location" className="form-label">
-                        Location
-                      </label>
-                      <select className="form-select" id="location">
-                        <option defaultValue={""}>Select location</option>
-                        <option value="East Purwokerto">East Purwokerto</option>
-                        <option value="West Purwokerto">West Purwokerto</option>
-                      </select>
-                    </div>
-                    <div className="row mb-4">
-                      <div className="col-6">
-                        <label htmlFor="date-start" className="form-label">
-                          Date Start
-                        </label>
-                        <input type="date" className="form-control" id="date-start" />
-                      </div>
-                      <div className="col-6">
-                        <label htmlFor="date-end" className="form-label">
-                          Date End
-                        </label>
-                        <input type="date" className="form-control" id="date-end" />
-                      </div>
-                    </div>
-                    <div className="mb-4">
-                      <label htmlFor="location" className="form-label">
-                        Time
-                      </label>
-                      <div className="row row-cols-4 gy-3">
-                        <div className="col">
-                          <button className="btn btn-outline-primary w-100 py-1">
-                            <i className="bi bi-plus-lg"></i>
-                          </button>
-                        </div>
-                        <div className="col">
-                          <button className="btn text-nowrap w-100 py-1">08.00am</button>
-                        </div>
-                        <div className="col">
-                          <button className="btn text-nowrap w-100 py-1">08.00am</button>
-                        </div>
-                        <div className="col">
-                          <button className="btn text-nowrap w-100 py-1">08.00am</button>
-                        </div>
-                        <div className="col">
-                          <button className="btn text-nowrap w-100 py-1">08.00am</button>
-                        </div>
-                        <div className="col">
-                          <button className="btn text-nowrap w-100 py-1">08.00am</button>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
                 </div>
               </div>
-            </div>
-            <div className="row buttons justify-content-end">
-              <div className="col-2">
-                <button className="btn btn-outline-primary w-100">Reset</button>
+              <div className="row buttons justify-content-end">
+                <div className="col-2">
+                  <button type="reset" className="btn btn-outline-primary w-100">
+                    Reset
+                  </button>
+                </div>
+                <div className="col-2">
+                  <button type="submit" className="btn btn-primary fw-semibold w-100">
+                    Submit
+                  </button>
+                </div>
               </div>
-              <div className="col-2">
-                <button className="btn btn-primary fw-semibold w-100">Submit</button>
-              </div>
-            </div>
+            </form>
           </div>
         </section>
 
@@ -201,31 +278,12 @@ function ManageSchedule() {
           </div>
           <div className="container-lg">
             <div className="card border-0 p-5">
-              <div className="row g-5 px-3 py-2">
-                <div className="col-3">
-                  <Card />
-                </div>
-                <div className="col-3">
-                  <Card />
-                </div>
-                <div className="col-3">
-                  <Card />
-                </div>
-                <div className="col-3">
-                  <Card />
-                </div>
-                <div className="col-3">
-                  <Card />
-                </div>
-                <div className="col-3">
-                  <Card />
-                </div>
-                <div className="col-3">
-                  <Card />
-                </div>
-                <div className="col-3">
-                  <Card />
-                </div>
+              <div className="row row-cols-3 g-5 px-3 py-2">
+                {schedules.data.map((schedule) => (
+                  <div className="col" key={schedule.id}>
+                    <ScheduleCard data={schedule} dataOrder={{}} pathname={location.pathname} />
+                  </div>
+                ))}
               </div>
             </div>
             <div className="d-flex justify-content-center mt-4 mb-2">
@@ -233,7 +291,7 @@ function ManageSchedule() {
                 previousLabel={"Previous"}
                 nextLabel={"Next"}
                 breakLabel={"..."}
-                pageCount={pageInfo.totalPage}
+                pageCount={schedules.pageInfo.totalPage}
                 onPageChange={handlePagination}
                 containerClassName={"pagination"}
                 pageClassName={"page-item px-1"}

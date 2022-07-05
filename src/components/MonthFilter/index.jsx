de9add86b3dbc21fe84ec2b3bba8029a10282ed0
Thariq Farsha <mthariqfarsha@gmail.com>
2022-07-05 22:40:56 +0700

    Fix month filter

--- src/components/MonthFilter/index.jsx
+++ src/components/MonthFilter/index.jsx
@@ -1,39 +1,45 @@
 import React, { useState } from "react";
 import "./index.css";
 
-function MonthFilter() {
+function MonthFilter(props) {
   const months = [
-    { number: 1, monthName: "January" },
-    { number: 2, monthName: "February" },
-    { number: 3, monthName: "March" },
-    { number: 4, monthName: "April" },
-    { number: 5, monthName: "May" },
-    { number: 6, monthName: "June" },
-    { number: 7, monthName: "July" },
-    { number: 8, monthName: "August" },
-    { number: 9, monthName: "September" },
-    { number: 10, monthName: "October" },
-    { number: 11, monthName: "November" },
-    { number: 12, monthName: "December" }
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
   ];
 
-  const month = new Date().getMonth() + 1;
-  const previous = months.splice(0, month);
-  const monthFromNow = [...months, ...previous];
+  const upcomingMonths = [
+    ...months.slice(props.firstMonth, months.length),
+    ...months.slice(0, props.firstMonth)
+  ];
 
-  const [selectedMonth, setSelectedMonth] = useState(new Date().getMonth() + 1);
+  const selectedMonth = months[props.month];
+
+  const handleSetMonth = (month) => {
+    const index = months.indexOf(month);
+    props.setMonth(index);
+  };
 
   return (
     <div className="month-filter container-lg month-wrapper mb-5">
-      {monthFromNow.map((month) => (
-        <span key={month.number}>
+      {upcomingMonths.map((month) => (
+        <span key={month}>
           <button
             className={`btn py-2 my-2 ${
-              selectedMonth === month.number ? "btn-primary" : "btn-outline-primary"
+              selectedMonth === month ? "btn-primary" : "btn-outline-primary"
             }`}
-            onClick={() => setSelectedMonth(month.number)}
+            onClick={() => handleSetMonth(month)}
           >
-            {month.monthName}
+            {month}
           </button>
         </span>
       ))}

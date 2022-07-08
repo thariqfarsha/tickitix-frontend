2a413f4122e9e151289979fa797c309741e3c34c
Thariq Farsha <mthariqfarsha@gmail.com>
2022-07-08 09:24:52 +0700

    Get user data for private route from redux

--- src/helpers/route/PrivateRoute.js
+++ src/helpers/route/PrivateRoute.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { useLocation, Navigate, Outlet } from "react-router-dom";
 
 export default function PrivateRoute(props) {
   const location = useLocation();
   const token = localStorage.getItem("token");
-  const dataUser = JSON.parse(localStorage.getItem("dataUser"));
+
+  const dataUser = useSelector((state) => state.user.data);
 
   if (!token) {
     return <Navigate to="/signin" state={{ from: location }} replace />;

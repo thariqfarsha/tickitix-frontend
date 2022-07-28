f870d51bc2600fa7f261744c56ed116d3e952ffc
Thariq Farsha <mthariqfarsha@gmail.com>
2022-07-28 09:43:42 +0700

    Remove basic pages route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import BasicCounter from "./pages/basic/Counter/classComponent";
-import BasicReact from "./pages/basic/React";
-import BasicLogin from "./pages/basic/Login";
-import BasicHome from "./pages/basic/Home";
 import Home from "./pages/Home";
 import SignIn from "./pages/Signin";
 import SignUp from "./pages/Signup";
@@ -26,11 +22,6 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="basic/counter" element={<BasicCounter />} />
-        <Route path="basic/react" element={<BasicReact />} />
-        <Route path="basic/login" element={<BasicLogin />} />
-        <Route path="basic/home" element={<BasicHome />} />
-
         <Route path="/" element={<Home />} />
         <Route path="detail/:id" element={<MovieDetails />} />
         <Route path="list-movie" element={<ListMovie />} />
